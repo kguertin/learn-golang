0d8537427e048dd08925748d09cf70fdbc97c145
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-05-25 10:22:10 -0400

    create shuffle function

--- cards/deck.go
+++ cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of deck which is a slice of strings
@@ -56,5 +58,15 @@ func newDeckFromFile(filename string) deck {
 
 	return deck(s)
 
-
 }
+
+func (d deck) shuffle() {
+	source := rand.NewSource(int64(time.Now().UnixNano()))
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+} 
