11a66ffb212d1a4f577b45db6cfca36747277b46
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-12 14:28:43 -0400

    typed and untyped constants

--- go-workspace/002-exercises/expressions/main.go
+++ go-workspace/002-exercises/expressions/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+const (
+	num1     = 13
+	num2 int = 666
+)
+
 func main() {
 	a := 5 == 5
 	fmt.Println(a)
@@ -15,5 +20,8 @@ func main() {
 	fmt.Println(e)
 	f := 5 > 10
 	fmt.Println(f)
-	
+
+	fmt.Printf("%v\t%T\n", num1, num1)
+	fmt.Printf("%v\t%T\n", num2, num2)
+
 }
