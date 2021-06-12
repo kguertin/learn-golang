98650069f8d67e41a31bf90e6b7812842effb30e
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-12 14:25:11 -0400

    expressions in go

---
+++ go-workspace/002-exercises/expressions/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5 == 5
+	fmt.Println(a)
+	b := 3 <= 5
+	fmt.Println(b)
+	c := 3 >= 5
+	fmt.Println(c)
+	d := 5 != 5
+	fmt.Println(d)
+	e := 5 < 10
+	fmt.Println(e)
+	f := 5 > 10
+	fmt.Println(f)
+	
+}
