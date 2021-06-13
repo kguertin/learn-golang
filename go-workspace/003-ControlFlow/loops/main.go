430cfd95f562b64bc8436d9c79757954ff56f925
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-06-13 11:45:51 -0400

    nested loops

--- go-workspace/003-ControlFlow/loops/main.go
+++ go-workspace/003-ControlFlow/loops/main.go
@@ -1,10 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	// for init; condtion; post;{
 	// }
 
 	for i := 0; i <= 10; i++ {
-		println(i)
+		for j := 0; j < 3; j++ {
+			fmt.Printf("The outer loop: %v\t The inner loop: %d\n", i, j)
+		}
 	}
 }
